e6e1849381816ce0fd4c0581e71d033129a7cb32
kobayashiukyo <u.k.knua0419@gmail.com>
2020-09-24 23:59:25 +0900

    add_order_count

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php require_once('data.php') ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Café Practice</title>
+  <link rel="stylesheet" type="text/css" href="stylesheet.css">
+  <link href='https://fonts.googleapis.com/css?family=Pacifico|Lato' rel='stylesheet' type='text/css'>
+</head>
+<body>
+  <div class="menu-wrapper container">
+    <h1 class="logo">Café Practice</h1>
+    <div class="menu-items">
+      <?php foreach ($menus as $menu): ?>
+        <div class="menu-item">
+          <img src="<?php echo $menu->getImage() ?>" class="menu-item-image">
+          <h3 class="menu-item-name"><?php echo $menu->getName() ?></h3>
+          <p class="price">¥<?php echo $menu->getTaxIncludedPrice() ?>（税込）</p>
+          <p>注文数: <?php echo $menu->getOrderCount() ?></p>
+        </div>
+      <?php endforeach ?>
+    </div>
+  </div>
+</body>
+</html>
